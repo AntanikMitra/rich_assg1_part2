a8819ca1774537b50ef5c46bafa46f61c357745c
antanikmitra <antanikmitra@hotmail.com>
2016-02-22 23:24:14 -0500

    changing footer

--- views/partials/footer.js
+++ views/partials/footer.js
@@ -2,16 +2,14 @@
           <div class="container">
             <div class="row">
               <div class="col l6 s12">
-                <h5 class="white-text">ANNTANIK MITRA</h5>
+                <h5 class="white-text">ANTANIK MITRA</h5>
                 <p class="grey-text text-lighten-4">Always there to help and guide you.</p>
               </div>
               <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Links</h5>
+                <h5 class="white-text">Connect</h5>
                 <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!"><b>f</b></a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!"><b>in</b></a></li>
                 </ul>
               </div>
             </div>
