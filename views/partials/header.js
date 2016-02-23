638ac9dbd7300337f3af1638aa3e1f8cc5f84c3e
antanikmitra <antanikmitra@hotmail.com>
2016-02-22 20:03:32 -0500

    nav bar update

--- views/partials/header.js
+++ views/partials/header.js
@@ -1,21 +1,34 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title><%= title %></title>
-    <link rel="stylesheet" href="/stylesheets/bootstrap.min.css"/>
-    <link rel="stylesheet" href="/stylesheets/bootstrap-theme.min.css"/>
-    <link rel="stylesheet" href='/stylesheets/style.css' />
-    
-    <script src="/javascripts/jquery-2.2.0.min.js"></script>
-    <script src="/javascripts/bootstrap.min.js"></script>
-  </head>
-<body>
-  <nav class="navbar navbar-inverse">
-    <ul class="nav navbar-nav">
-        <li><a href="/" title="Home">Home</a></li>
-        <li><a href="/about" title="About">About</a></li>
-        <li><a href="/projects" title="Projects">Projects</a></li>
-         <li><a href="/services" title="Services">Services</a></li>
-          <li><a href="/contact" title="Contact">Contact</a></li>
-   </ul>
-  </nav>
+<head>
+<title><%= title %></title>
+
+
+< link rel= "stylesheet" href= '/stylesheets/style.css' />
+<link rel="stylesheet" href= '/stylesheets/css/materialize.min.css' /> 
+  
+<script src= "/javascripts/jquery-2.2.0.min.js" > </script>
+< script src= "/javascripts/materialize.min.js" > </script>
+< /head>
+< body >
+
+
+<div class="nav-wrapper" >
+<a href="#!" class="brand-logo" > Logo < /a>
+< a href= "#" data- activates="mobile-demo" class="button-collapse" > <i class="material-icons" > menu < /i></a >
+<ul class="right hide-on-med-and-down" >
+<li><a href="/" title= "Home" > Home < /a></li >
+<li><a href="/about" title= "About" > About < /a></li >
+<li><a href="/projects" title= "Projects" > Projects < /a></li >
+<li><a href="/services" title= "Services" > Services < /a></li >
+<li><a href="/contact" title= "Contact" > Contact < /a></li >
+</ul>
+< ul class="side-nav" id= "mobile-demo" >
+<li><a href="/" title= "Home" > Home < /a></li >
+<li><a href="/about" title= "About" > About < /a></li >
+<li><a href="/projects" title= "Projects" > Projects < /a></li >
+<li><a href="/services" title= "Services" > Services < /a></li >
+<li><a href="/contact" title= "Contact" > Contact < /a></li >
+</ul>
+< /div>
+< /nav>
